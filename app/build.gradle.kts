9d8a4db756929426c595f49109ae7ff9a44c2dc7
isanghoony <isanghoony@gmail.com>
2025-12-07 11:46:30 +0900

    fix build error data.api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import com.ddd.attendance.setNamespace
 plugins {
     alias(libs.plugins.attendance.android.application)
     alias(libs.plugins.attendance.android.hilt)
-    alias(libs.plugins.attendance.android.compose) //임시
+    alias(libs.plugins.attendance.android.compose)
 }
 
 setNamespace("app")
