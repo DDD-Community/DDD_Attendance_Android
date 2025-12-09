989da657e3f22e9c73df425e70f6271ea0688485
isanghoony <isanghoony@gmail.com>
2025-12-09 18:15:00 +0900

    add navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,3 +7,7 @@ plugins {
 }
 
 setNamespace("app")
+
+dependencies {
+    implementation(projects.feature.home)
+}
---
+++ feature/home/src/main/kotlin/com/ddd/attendance/feature/home/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.ddd.attendance.feature.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+) {
+    Text("네비게이션 구현")
+}
