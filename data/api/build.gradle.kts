9e701d7bbf2822c5fe56d6839c5a1e16bcd29bfe
osemin <semin015123@gmail.com>
2025-12-15 21:12:19 +0900

    remove todo annotation

--- data/api/build.gradle.kts
+++ data/api/build.gradle.kts
@@ -4,10 +4,6 @@ plugins {
     id("attendance.android.library")
     id("attendance.android.hilt")
     id("attendance.android.retrofit")
-    // todo find reason for cannot use alias
-    // alias(libs.plugins.attendance.android.library)
-    // alias(libs.plugins.attendance.android.hilt)
-    // alias(libs.plugins.attendance.android.retrofit)
 }
 
 setNamespace("data.api")
