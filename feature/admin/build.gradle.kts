95d91b81288f97270d2e6eae0452ff0304a74e86
isanghoony <isanghoony@gmail.com>
2025-12-22 17:56:57 +0900

    add feature:admin module

---
+++ feature/admin/build.gradle.kts
@@ -0,0 +1,9 @@
+import com.ddd.attendance.setNamespace
+
+plugins {
+    alias(libs.plugins.attendance.android.library)
+    alias(libs.plugins.attendance.android.hilt)
+    alias(libs.plugins.attendance.android.compose)
+}
+
+setNamespace("feature.admin")
