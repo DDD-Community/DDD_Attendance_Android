5050fad1ce1375d4a0393ffdd7383d3281baffe6
osemin <semin015123@gmail.com>
2026-01-02 23:24:56 +0900

    add qr screen

--- feature/core/src/main/java/com/ddd/attendance/feature/core/qr/util/QrCodeGenerator.kt
+++ feature/core/src/main/java/com/ddd/attendance/feature/core/qr/util/QrCodeGenerator.kt
@@ -3,6 +3,7 @@ package com.ddd.attendance.feature.core.qr.util
 import android.graphics.Bitmap
 import android.graphics.Color
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.WriterException
 import com.google.zxing.common.BitMatrix
@@ -16,7 +17,8 @@ internal class QrCodeGenerator {
     ): Result<Bitmap> {
         return try {
             val writer = MultiFormatWriter()
-            val bitMatrix: BitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, width, height)
+            val hints = hashMapOf<EncodeHintType, Any>(EncodeHintType.MARGIN to 0)
+            val bitMatrix: BitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, width, height, hints)
             val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
             
             for (x in 0 until width) {
--- feature/member/attendance/build.gradle.kts
+++ feature/member/attendance/build.gradle.kts
@@ -10,4 +10,5 @@ setNamespace("feature.member.attendance")
 
 dependencies {
     implementation(projects.feature.designsystem)
+    implementation(projects.feature.core)
 }
